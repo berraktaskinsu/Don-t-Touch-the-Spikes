0b17464caaa71c58a5177d39a9fd0605c291f7f2
berraktaskinsu <berraktaskinsu@gmail.com>
2021-08-27 23:29:47 +0300

    F-1: Create Spiky jump animation

--- Assets/Scripts/HitPlayer.cs
+++ Assets/Scripts/HitPlayer.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class HitPlayer : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D other) {
+    void OnCollisionEnter2D(Collision2D other)
+    {
         switch (other.gameObject.tag)
         {
-            case "Wall": {
-                GetComponent<MovePlayer>().speed *= -1;
-            Debug.Log("Wall");
-                break;
-            }
-            case "Spike": {
-                Destroy(this);
-                Debug.Log("Game Over");
-                Destroy(GetComponent<MovePlayer>());
-                Destroy(gameObject, 5);
-                break;
-            }
-            case "Candy": {
-                break;
-            }
+            case "Wall":
+                {
+                    GetComponent<MovePlayer>().speed *= -1;
+                    Debug.Log("Wall");
+                    break;
+                }
+            case "Spike":
+                {
+                    Destroy(this);
+                    Debug.Log("Game Over");
+                    Destroy(GetComponent<MovePlayer>());
+                    Destroy(gameObject, 5);
+                    break;
+                }
+            case "Candy":
+                {
+                    break;
+                }
             default:
                 break;
         }
--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -7,15 +7,18 @@ public class MovePlayer : MonoBehaviour
     [SerializeField] public float speed = 0.02f;
     [SerializeField] float jumpVelocity = 10f;
     private Rigidbody2D rigidBody;
+    private Animator animator;
 
     void Awake()
     {
         rigidBody = transform.GetComponent<Rigidbody2D>();
+
     }
 
     void Start()
     {
         rigidBody.velocity = Vector2.up * jumpVelocity;
+        animator = gameObject.GetComponent<Animator>();
     }
 
     void Update()
@@ -23,7 +26,26 @@ public class MovePlayer : MonoBehaviour
         transform.Translate(speed, 0, 0);
         if (Input.GetKeyDown(KeyCode.Space))
         {
+
             rigidBody.velocity = Vector2.up * jumpVelocity;
+            float currentTime = animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+            if (currentTime >= 1f)
+            {
+                animator.Play("Jump", -1, 0f);
+                return;
+            }
+            int currentFrame = (int)Mathf.Ceil((currentTime * 100) / 11);
+            Debug.Log(currentTime + " " + currentFrame);
+            int nextFrame = currentFrame - 1;
+            if (currentFrame > 6)
+            {
+                // Restart from equivalent frame
+                nextFrame = 11 - currentFrame + 1;
+                Debug.Log(currentFrame + " " + nextFrame);
+
+            }
+            animator.Play("Jump", -1, (nextFrame * 11f) / 100f);
+
         }
     }
 }

a158bf85a187fb292d82faa34c85c0cd83afda9e
berraktaskinsu <berraktaskinsu@gmail.com>
2021-09-21 19:12:30 +0300

    F-2: Add transition animation to spikes

---
+++ Assets/Common/Enums/EMovement.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EMovement
+{
+    public enum Movement
+    {
+        Hide,
+        Show,
+        Idle
+    }
+}
--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    [SerializeField] public float speed = 0.02f;
-    [SerializeField] float jumpVelocity = 10f;
+    [SerializeField] public float speed = 5f;
+    [SerializeField] float jumpVelocity = 7f;
     private Rigidbody2D rigidBody;
     private Animator animator;
 
@@ -23,29 +23,31 @@ public class MovePlayer : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(speed, 0, 0);
+        transform.Translate(speed * Time.deltaTime, 0, 0);
         if (Input.GetKeyDown(KeyCode.Space))
         {
-
             rigidBody.velocity = Vector2.up * jumpVelocity;
-            float currentTime = animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
-            if (currentTime >= 1f)
-            {
-                animator.Play("Jump", -1, 0f);
-                return;
-            }
-            int currentFrame = (int)Mathf.Ceil((currentTime * 100) / 11);
-            Debug.Log(currentTime + " " + currentFrame);
-            int nextFrame = currentFrame - 1;
-            if (currentFrame > 6)
-            {
-                // Restart from equivalent frame
-                nextFrame = 11 - currentFrame + 1;
-                Debug.Log(currentFrame + " " + nextFrame);
+            RestartAnimation();
+        }
+    }
 
-            }
-            animator.Play("Jump", -1, (nextFrame * 11f) / 100f);
+    private void RestartAnimation()
+    {
+        float currentTime = animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+        if (currentTime >= 1f)
+        {
+            animator.Play("Jump", -1, 0f);
+            return;
+        }
+        int currentFrame = (int)Mathf.Ceil((currentTime * 100) / 11);
+
+        int nextFrame = currentFrame - 1;
+        if (currentFrame > 6)
+        {
+            // Restart from equivalent frame
+            nextFrame = 11 - currentFrame + 1;
 
         }
+        animator.Play("Jump", -1, (nextFrame * 11f) / 100f);
     }
 }
